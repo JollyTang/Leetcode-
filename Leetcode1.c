a790d77a44543962f827eb4c55feba9e46b7d164
JollyTang <87261510+JollyTang@users.noreply.github.com>
2022-02-23 17:48:17 +0800

    Add files via upload

---
+++ Leetcode1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target){
+    int* returned;
+    returned = (int*)malloc(2*sizeof(int));
+    for(int i = 0;i < numsSize;i++){
+        for(int j = i+1;j<numsSize;j++){
+            if(*(nums + i) + *(nums + j) == target){
+                *returned = i;
+                *(returned + 1) = j;
+                return returned;
+            }
+        }
+    }
+    return returned;
+}
+
+int main(){
+    int a[] = {2,7,11,15};
+    int* nums = a;
+    int* returned = twoSum(nums,4,9);
+    for(int i = 0;i<2;i++){
+        printf("%d ",*(returned + i));
+    }
+
+}
