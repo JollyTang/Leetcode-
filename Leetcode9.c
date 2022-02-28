a2d296a171d1031a6a1e6e3786b04100488df1d4
JollyTang <87261510+JollyTang@users.noreply.github.com>
2022-02-28 14:39:17 +0800

    Add files via upload

---
+++ Leetcode9.c
@@ -0,0 +1,22 @@
+bool isPalindrome(int x) {
+	if (x < 0) {
+		return false;
+	}
+	int temp = x;
+	int a[20] = { 0 };
+
+	int i = 0;
+	while (temp > 0) {
+		a[i++] = temp % 10;
+		temp -= temp % 10;
+		temp /= 10;
+	}
+	i--;
+	int j = 0;
+	for (; j < i;) {
+		if (a[j++] != a[i--]) {
+			return false;
+		}
+	}
+	return true;
+}
